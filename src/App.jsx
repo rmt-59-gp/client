664ebf10bceefa5971ed015e7e797fb8d8c868f5
zen-geohub <abidinzaenal556@gmail.com>
2025-04-08 20:46:06 +0700

    feat: protecting route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import LoginPage from "./pages/LoginPage";
+import QuizLayout from "./layouts/QuizLayout";
+import LobbyPage from "./pages/LobbyPage";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+
+        <Route element={<QuizLayout />}>
+          <Route path="/" element={<LobbyPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
