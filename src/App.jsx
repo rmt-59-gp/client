6591e3b752f500fc4998bc2608ff5a8b505e0159
RadenAchmadBobbySyakir <bobbysyakir18@gmail.com>
2025-04-08 21:41:14 +0700

    waiting room page, list of waiting user-name

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import LoginPage from "./pages/LoginPage";
+import WaitingRoomPage from "./pages/WaitingRoomPage";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/room/:id" element={<WaitingRoomPage />} />
       </Routes>
     </BrowserRouter>
   );
