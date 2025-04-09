7f0bba085e761b6dd5ccff11218809af0bf9f1bd
zen-geohub <abidinzaenal556@gmail.com>
2025-04-09 19:54:23 +0700

    feat: create room

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import LoginPage from "./pages/LoginPage";
 import QuizLayout from "./layouts/QuizLayout";
 import RoomBrowserPage from "./pages/RoomBrowserPage";
 import WaitingRoomPage from "./pages/WaitingRoomPage";
+import RoomCreate from "./pages/RoomCreate";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
 
         <Route element={<QuizLayout />}>
           <Route path="/" element={<RoomBrowserPage />} />
+          <Route path="/room/create" element={<RoomCreate />} />
           <Route path="/room/:id" element={<WaitingRoomPage />} />
         </Route>
       </Routes>
