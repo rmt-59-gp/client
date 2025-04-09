a3aaa0a643b2065a81e08c2760292465eb0943c2
RadenAchmadBobbySyakir <bobbysyakir18@gmail.com>
2025-04-09 09:45:22 +0700

    rotate border

--- src/pages/WaitingRoomPage.jsx
+++ src/pages/WaitingRoomPage.jsx
@@ -6,7 +6,6 @@ import { io } from "socket.io-client";
 
 const WaitingRoomPage = () => {
     const participantsEx = ["Alice", "Bob", "Charlie", "Diana"]; // Contoh
-    
     const [participants, setParticipants] = useState([]);
     
     useEffect(() => {
@@ -36,7 +35,7 @@ const WaitingRoomPage = () => {
                 <p className="text-center text-lg font-['Fredoka'] text-gray-700 mb-6">
                     Please wait while everyone joins the quiz!
                 </p>
-                <div className="bg-yellow-100 border-[3px] font-['Fredoka'] border-dashed border-pink-400 rounded-3xl shadow-[6px_6px_0_#00000040] p-6 rotate-[-1.5deg] mb-7">
+                <div className="bg-yellow-100 border-[3px] font-['Fredoka'] border-dashed border-pink-400 rounded-3xl shadow-[6px_6px_0_#00000040] p-6 rotate-[1.5deg] mb-7">
                     <h2 className="text-xl font-semibold mb-4 text-blue-800">Participants:</h2>
                     <ul className="list-disc list-inside">
                         {participantsEx.map((participant, index) => (
@@ -47,7 +46,7 @@ const WaitingRoomPage = () => {
                     </ul>
                 </div>
                 <div className="text-center">
-                    <button className="bg-pink-400 hover:cursor-pointer hover:bg-pink-500 font-['Fredoka'] text-white px-6 py-2 rounded-full shadow-md">
+                    <button className="bg-pink-400 hover:cursor-pointer hover:bg-pink-500 font-['Fredoka'] p-6 rotate-[1.5deg] text-white px-6 py-2 rounded-full shadow-md">
                         Start Quiz
                     </button>
                 </div>
